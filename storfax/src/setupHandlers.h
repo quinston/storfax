fb86448841ee23f838b3f5197b34acb4256d67fb
quinston <lklq@live.com>
2014-09-21 03:13:15 -0400

    Hold and move space ship

--- storfax/src/setupHandlers.h
+++ storfax/src/setupHandlers.h
@@ -1,7 +1,7 @@
 #pragma once
 
 static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+  window_single_repeating_click_subscribe(BUTTON_ID_SELECT, 30, select_click_handler);
+  window_single_repeating_click_subscribe(BUTTON_ID_UP,30, up_click_handler);
+  window_single_repeating_click_subscribe(BUTTON_ID_DOWN, 30, down_click_handler);
 }
